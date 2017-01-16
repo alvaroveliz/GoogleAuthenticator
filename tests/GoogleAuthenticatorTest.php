d0bdbd23b9a6855ad3570e51d406aa0e449adb41
Alvaro Véliz <alvaro.veliz@gmail.com>
2017-01-16 19:28:26 -0300

    Fixing style

--- tests/GoogleAuthenticatorTest.php
+++ tests/GoogleAuthenticatorTest.php
@@ -43,7 +43,7 @@ class GoogleAuthenticatorTest extends \PHPUnit_Framework_TestCase
     {
         $url = $this->helper->getUrl('foo', 'foobar.org', '3DHTQX4GCRKHGS55CJ', 'FooBar');
 
-        $expected = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/foo@foobar.org%3Fsecret%3D3DHTQX4GCRKHGS55CJ%26issuer%3DFooBar";
+        $expected = 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/foo@foobar.org%3Fsecret%3D3DHTQX4GCRKHGS55CJ%26issuer%3DFooBar';
 
         $this->assertEquals($expected, $url);
     }
